814bd3f5163cd6d691f8878737aacc47dcd691c7
deepshikha <deepshikhaverma44@gmail.com>
2024-02-28 12:12:45 +0530

    Login component design

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import { Button, Form, Input } from 'antd';
+import { Link } from 'react-router-dom';
 
 function Login() {
   return (
-    <div>Login</div>
+    <>
+      <header className='App-header'>
+        <main className='main-area mw-500 text-center px-3'>
+          <section className='left-section'>
+            <h1>Login BookMyShow</h1>
+          </section>
+          <section className='right-section'>
+            <Form layout='vertical'>             
+              
+              <Form.Item label='Email' htmlFor='email' name="email" className='d-block' rules={[{ required: true, message: 'Email is required' }]}>
+                <Input id="email" type="text" placeholder="Enter your Email" />
+              </Form.Item>
+
+              <Form.Item label='Password' htmlFor='password' name="password" className='d-block' rules={[{ required: true, message: 'Password is required' }]}>
+                <Input id="password" type="password" placeholder="Enter your Password" />
+              </Form.Item>
+
+              <Form.Item>
+                <Button type='primary' block htmlType="submit" style={{fontSize:"1rem",fontWeight:"600"}}>Login</Button>
+              </Form.Item>
+            </Form>
+            <div>
+              <p>
+                New User ? <Link to="/register">Register Now </Link>
+              </p>
+            </div>
+          </section>
+        </main>
+      </header>
+
+
+    </>
   )
 }
 
